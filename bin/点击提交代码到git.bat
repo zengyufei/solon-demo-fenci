e30fdc168b0a1480323a657ac5bb1c9eb31c2687
zengyufei <31636208@qq.com>
2024-03-21 10:04:53 +0800

    update 21周四 22403_100453

--- bin/点击提交代码到git.bat
+++ bin/点击提交代码到git.bat
@@ -5,7 +5,7 @@ cd ..
 echo %cd%
 
 git add ./*
-git commit -m "update %date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "update %date:~11,2%%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
 
 cd ./bin
